feb1b7701611ac839321daaa3cd1be534c96d02e
the-dagger <harshithdwivedi@gmail.com>
2018-07-05 19:33:10 +0530

    add code for menus
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

---
+++ Lesson11/Menus/app/src/main/java/com/codingblocks/menus/MainActivity.java
@@ -0,0 +1,63 @@
+package com.codingblocks.menus;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi = getMenuInflater();
+        // Inflate the menu; this adds items to the action bar if it is present.
+        mi.inflate(R.menu.menu_main, menu);
+
+        //Return true to display the menu on the screen
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()){
+            case R.id.action_about :
+                //Do something
+                break;
+            case R.id.action_help :
+                //DO something else
+                break;
+        }
+
+
+        //Return true to finish the click handling to complete here
+        //Return false to allow the click action to progress to the
+        // views hidden behind the menu action
+        return true;
+    }
+}
